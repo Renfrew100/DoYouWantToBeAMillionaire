60600ba1726e6dd8c1dbf9fef2c49f9a02b8849a
Ali Aftab Muhammad <37605427+Renfrew100@users.noreply.github.com>
2018-04-20 13:47:13 -0400

    InitialCommit

---
+++ doyouwanttobeamillionaire/DoYouWantToBeAMillionaire.java
@@ -0,0 +1,21 @@
+
+package doyouwanttobeamillionaire;
+
+
+public class DoYouWantToBeAMillionaire {
+
+    public static void main(String[] args) {
+    //MainScreen form = new MainScreen();
+    //form.setVisible(true);
+    //form.setSize(1366,768);
+    
+    Instructions form1 = new Instructions();
+    form1.setVisible(true);
+    form1.setSize(1366,768);
+    
+    
+    
+     
+    }
+    
+}
