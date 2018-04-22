925f423d57512bf5f67181b3cbcbfea5b502ab44
Justgo13 <37590812+Justgo13@users.noreply.github.com>
2018-04-22 12:40:42 -0400

    Added all screens (rough)
    Also can move between screens with use of buttons

---
+++ Millionaire/DoYouWantToBeAMillionaire.java
@@ -0,0 +1,21 @@
+
+package Millionaire;
+
+
+public class DoYouWantToBeAMillionaire {
+
+    public static void main(String[] args) {
+    //MainScreen form = new MainScreen();
+    //form.setVisible(true);
+    //form.setSize(1366,768);
+    
+    Instructions form1 = new Instructions();
+    form1.setVisible(true);
+    form1.setSize(1366,768);
+    
+    
+    
+     
+    }
+    
+}
