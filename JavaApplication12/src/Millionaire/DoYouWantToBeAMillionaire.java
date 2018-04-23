917cb161f184c1dbf99e16ad9d1a0c5eb6931aaa
MustafaAli789 <37590511+MustafaAli789@users.noreply.github.com>
2018-04-23 15:04:54 -0400

    started working on lifelife 1 method

--- JavaApplication12/src/Millionaire/DoYouWantToBeAMillionaire.java
+++ JavaApplication12/src/Millionaire/DoYouWantToBeAMillionaire.java
@@ -5,8 +5,37 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 import java.util.List;
+import java.util.Random;
 
 public class DoYouWantToBeAMillionaire {
+    
+    public static void lifeLine1(int questionNumber){
+        Random rand = new Random();
+        ArrayList<String> incorrectAnswer = new ArrayList<String>();
+        switch(loadQuestions().get(questionNumber - 1).get(5)){
+            case "A":
+                incorrectAnswer.add("B");
+                incorrectAnswer.add("C");
+                incorrectAnswer.add("D");
+            case "B":
+                incorrectAnswer.add("A");
+                incorrectAnswer.add("C");
+                incorrectAnswer.add("D");
+            case "C":
+                incorrectAnswer.add("A");
+                incorrectAnswer.add("B");
+                incorrectAnswer.add("D");
+            case "D":
+                incorrectAnswer.add("A");
+                incorrectAnswer.add("B");
+                incorrectAnswer.add("C");
+        for (int i = 0; i < 2; i++){
+            int  randNum = rand.nextInt(2) + 0;
+            
+        }    
+            
+        }
+    }
     
     public static ArrayList<ArrayList<String>> loadQuestions(){
         int questionNum = 4;
